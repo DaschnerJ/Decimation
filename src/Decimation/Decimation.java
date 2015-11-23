3b9716861f96c2d76b3cbaa550282da5728fa36f
Damien <Damien@Acer-laptop1.PK5001Z>
2015-11-23 02:12:00 -0800

    Added LWJGL 3 to libraries.

--- src/Decimation/Decimation.java
+++ src/Decimation/Decimation.java
@@ -1,10 +1,12 @@
 package Decimation;
 
+
 public class Decimation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Test");
+		
 	}
 
 }
